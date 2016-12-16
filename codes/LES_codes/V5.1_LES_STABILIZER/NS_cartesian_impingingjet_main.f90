7aa61272d895998a0e63ef1982032cc39fa93d13
RamanathanVaradharajan <ramanathan.varadharajan@wur.nl>
2016-12-16 22:01:39 +0100

    Made subroutines to initialize, read file and write file. Code is renamed to ane_Watsons'(Just ANother Efficient and Wide Algorithm To SOlve Navier Stokes). Just for fun.

--- codes/LES_codes/V5.1_LES_STABILIZER/NS_cartesian_impingingjet_main.f90
+++ codes/LES_codes/V5.1_LES_STABILIZER/NS_cartesian_impingingjet_main.f90
@@ -1,7 +1,4 @@
-        !3D Navier stokes algorithm using explicit filtered LES (for turbulent jet flow)
-        !Main Program
-        !______________________________________________________________________
-        PROGRAM Jet
+        PROGRAM Jane_Watsons ! Just ANother Efficient and Wide Algorithm To Solve Navier Stokes
 
         USE GRID_DIMENSIONS, ONLY : NXL,NXR,NYB,NYT,NZB,NZF
         USE GRID_SIZE_TIME_STEP, ONLY : dt,jet_d
@@ -65,187 +62,74 @@
         WRITE(*,*)'******************************************************************'
         
 
-        write(*,*)  'Initialization'
-        CALL INITIALIZATION
-        CALL DENSITY_INITIALIZATION
-        CALL U_VELOCITY_INITIALIZATION
-        CALL V_VELOCITY_INITIALIZATION
-        CALL PRESSURE_INITIALIZATION
-        CALL ENERGY_INITIALIZATION
-        CALL VORTICITY_INITIALIZATION
-        CALL LIGHTHILL_INITIALIZATION
-        CALL INITIALIZE_FILTERING_COEFF_X
-        CALL INITIALIZE_FILTERING_COEFF_Y
-        CALL INITIALIZE_FILTERING_COEFF_Z
-        CALL GRID_INITIALIZATION
-        CALL INITIALIZE_BUFFER_FILTER
-        CALL METRICS_X
-        CALL METRICS_Y
-        CALL METRICS_Z
-        CALL INITIALIZE_RANDOM_NUMBERS_FREUND
-        
-
-        WRITE(*,*) 'Initialization Done'
-
+       
+		CALL INIT
         temp=i
-
-	IF (i .ne. 0) then
-        CALL READ_FILE(i)
-        CALL READ_DENSITY(i)
-        CALL READ_U_VELOCITY(i)
-        CALL READ_V_VELOCITY(i)
-        CALL READ_W_VELOCITY(i)
-        CALL READ_PRESSURE(i)
-        CALL READ_ENERGY(i)
-        CALL READ_VORTICITY(i)
-        CALL READ_LIGHTHILL_SOURCE(i)
-	CALL READ_INFLOW_FREUND(i)
+		IF (i .ne. 0) then
+			CALL READFILE(i)
         END IF
-        
+      
         CALL PRODUCT_OF_TWO_TERMS(rho_cur,u_cur,rhou_cur)
         CALL PRODUCT_OF_TWO_TERMS(rho_cur,v_cur,rhov_cur)
         CALL PRODUCT_OF_TWO_TERMS(rho_cur,w_cur,rhow_cur)
-        
-        IF(j.ne.0)THEN
-	i = j
-        CALL READ_MEAN_DENSITY(i)
-        CALL READ_MEAN_U_VELOCITY(i)
-        CALL READ_MEAN_V_VELOCITY(i)
-        CALL READ_MEAN_W_VELOCITY(i)
-        CALL READ_MEAN_PRESSURE(i)
-        CALL READ_MEAN_ENERGY(i)
-        CALL READ_MEAN_LIGHTHILL_SOURCE(i)
-        CALL READ_MEAN_CROSS_MOMENTUM(i)
-        CALL READ_MEAN_RHOU_MOMENTUM(i)
-        CALL READ_MEAN_RHOV_MOMENTUM(i)
-        CALL READ_MEAN_RHOW_MOMENTUM(i)
-        CALL READ_MEAN_TURB_KE(i)
-        !CALL CALCULATE_MEAN_P_RHO_GRAD(l)
-        CALL READ_TOTAL(i)
-	END IF
 
-	i =temp
-
+		IF(j.ne.0)THEN
+			i = j
+			CALL READMEAN(i)
+		END IF
+		i =temp
         CALL CPU_TIME(start_time)
 
         DO
-        IF(i >= laststep) EXIT
+        	IF(i >= laststep) EXIT
 
-        !SOLUTION BLOWUP CONTROLLER
-        !**************************
+        		!SOLUTION BLOWUP CONTROLLER
+        		!**************************
         
-	IF(i .GT. meanorig)THEN
-        meanstart = meanorig
-        ELSE
-        meanstart=0
-        END IF
-        !***************************
+				IF(i .GT. meanorig)THEN
+        		meanstart = meanorig
+        		ELSE
+        		meanstart=0
+        		END IF
+        		!***************************
         
-	CALL TOTAL_VALUE
-        CALL TOTAL_SQUARE_VALUE
-        CALL INFLOW_FREUND(i)
-        CALL RK2(i)
+			CALL TOTAL_VALUE
+        	CALL TOTAL_SQUARE_VALUE
+        	CALL INFLOW_FREUND(i)
+        	CALL RK2(i)
+        	i = i + 1
+        	mi = mi + 1
+			
+	        IF(i.le.meanstart)THEN
+        		rem = MOD(i,data_write_every)
+        	ELSE
+        		rem = MOD(i,mean_write_every)
+        	END IF
 
-        i = i + 1
-        mi = mi + 1
-       
-
-        IF(i.le.meanstart)THEN
-        rem = MOD(i,data_write_every)
-        ELSE
-        rem = MOD(i,mean_write_every)
-        END IF
-
-        IF (rem == 0 ) THEN
-
-        CALL WRITE_INFLOW_FREUND(i)
-
-        CALL WRITE_DENSITY(i)
-        CALL WRITE_U_VELOCITY(i)
-        CALL WRITE_V_VELOCITY(i)
-        CALL WRITE_W_VELOCITY(i)
-        CALL WRITE_PRESSURE(i)
-        CALL WRITE_ENERGY(i)
-        CALL WRITE_VORTICITY(i)
-        CALL WRITE_LIGHTHILL_SOURCE(i)
+        	IF (rem == 0 ) THEN
+				CALL WRITEFILE(i)
+	        END IF
         
-        CALL WRITE_DENSITY_XY_PROFILE(i)
-        CALL WRITE_U_VELOCITY_XY_PROFILE(i)
-        CALL WRITE_V_VELOCITY_XY_PROFILE(i)
-        CALL WRITE_W_VELOCITY_XY_PROFILE(i)
-        CALL WRITE_PRESSURE_XY_PROFILE(i)
-        CALL WRITE_ENERGY_XY_PROFILE(i)
-        CALL WRITE_LIGHTHILL_SOURCE_XY_PROFILE(i)
-        CALL WRITE_TEMPERATURE_XY_PROFILE(i)
-        CALL WRITE_VORTICITY_XY_PROFILE(i)
-
-        CALL WRITE_DENSITY_XZ_PROFILE(i)
-        CALL WRITE_U_VELOCITY_XZ_PROFILE(i)
-        CALL WRITE_V_VELOCITY_XZ_PROFILE(i)
-        CALL WRITE_W_VELOCITY_XZ_PROFILE(i)
-        CALL WRITE_PRESSURE_XZ_PROFILE(i)
-        CALL WRITE_ENERGY_XZ_PROFILE(i)
-        CALL WRITE_VORTICITY_XZ_PROFILE(i)
-        
-        CALL WRITE_DENSITY_YZ_PROFILE(i)
-        CALL WRITE_U_VELOCITY_YZ_PROFILE(i)
-        CALL WRITE_V_VELOCITY_YZ_PROFILE(i)
-        CALL WRITE_W_VELOCITY_YZ_PROFILE(i)
-        CALL WRITE_PRESSURE_YZ_PROFILE(i)
-        CALL WRITE_ENERGY_YZ_PROFILE(i)
-        CALL WRITE_VORTICITY_YZ_PROFILE(i)
-	CALL WRITE_INFLOW_FREUND(i)
-
-        CALL CALCULATE_MONITORPOINTS(mi)
-        CALL CALCULATE_MONITORLINES(mi)
-        CALL CALCULATE_LIGHTHILL(i)
-
-        WRITE(*,*) 'STEP',i,'     COMPLETED...   FILES ARE WRITTEN'
-        END IF
-        
-        OPEN(UNIT=19,FILE='run_monitor_log.dat',ACTION='WRITE',POSiTION='APPEND')
-        write(19,*) 'Step',i,'u(0,0,0)',u_cur(0,0,0)
-        CLOSE(19)
+			OPEN(UNIT=19,FILE='run_monitor_log.dat',ACTION='WRITE',POSiTION='APPEND')
+			write(19,*) 'Step',i,'u(0,0,0)',u_cur(0,0,0)
+			CLOSE(19)
          
 
-	IF(i > meanstart) THEN
-        j = i - meanstart
+			IF(i > meanstart) THEN
+        		j = i - meanstart
        
                 IF(i .lt. meanorig)THEN
-                j = 4000
-                write(*,*) 'success'
+                	j = 4000
+                	write(*,*) 'success'
                 END IF
 
-        rem = MOD(j,mean_write_every)
+        		rem = MOD(j,mean_write_every)
        
-        IF (rem == 0 ) THEN
-        !
-        CALL MEAN_VALUE(j)
-        CALL MEAN_SQUARE_VALUE(j)
-        !
-        CALL WRITE_MEAN_DENSITY(j)
-        CALL WRITE_MEAN_U_VELOCITY(j)
-        CALL WRITE_MEAN_V_VELOCITY(j)
-        CALL WRITE_MEAN_W_VELOCITY(j)
-        CALL WRITE_MEAN_PRESSURE(j)
-        CALL WRITE_MEAN_ENERGY(j)
-        CALL WRITE_MEAN_LIGHTHILL_SOURCE(j)
-        CALL WRITE_MEAN_CROSS_MOMENTUM(j)
-        CALL WRITE_MEAN_RHOU_MOMENTUM(j)
-        CALL WRITE_MEAN_RHOV_MOMENTUM(j)
-        CALL WRITE_MEAN_RHOW_MOMENTUM(j)
-        CALL WRITE_MEAN_TURB_KE(j)
-        !
-        CALL READ_TOTAL(j)
-        !
-        CALL WRITE_MONITORPOINTS(j)
-        CALL WRITE_MONITORLINES(j)
-        WRITE(*,*) 'MEAN DATA WRITTEN FOR',J,'SAMPLES'
-        !
-        mi = 0
-        END IF
-        END IF
-
+        		IF (rem == 0 ) THEN
+					CALL WRITEMEAN(j)
+			        mi = 0
+        		END IF
+        	END IF
 
         END DO
 
@@ -253,8 +137,7 @@
         CALL CPU_TIME(end_time)
         PRINT *, 'TIME', end_time - start_time
         WRITE(*,*) 'LES SIMULATION - ROUNDJET: COMPLETED SUCCESSFULLY'
-        END PROGRAM Jet
-
+        END PROGRAM Jane_Watsons
 
 
 
@@ -322,3 +205,135 @@
         END SUBROUTINE
         !______________________________________________________________________
 
+
+
+		SUBROUTINE INIT
+		
+			CALL INITIALIZATION
+			CALL DENSITY_INITIALIZATION
+			CALL U_VELOCITY_INITIALIZATION
+			CALL V_VELOCITY_INITIALIZATION
+			CALL PRESSURE_INITIALIZATION
+			CALL ENERGY_INITIALIZATION
+			CALL VORTICITY_INITIALIZATION
+			CALL LIGHTHILL_INITIALIZATION
+			CALL INITIALIZE_FILTERING_COEFF_X
+			CALL INITIALIZE_FILTERING_COEFF_Y
+			CALL INITIALIZE_FILTERING_COEFF_Z
+			CALL GRID_INITIALIZATION
+			CALL INITIALIZE_BUFFER_FILTER
+			CALL METRICS_X
+			CALL METRICS_Y
+			CALL METRICS_Z
+			CALL INITIALIZE_RANDOM_NUMBERS_FREUND
+			
+		END SUBROUTINE INIT
+		
+		
+		SUBROUTINE READFILE(k)
+			integer, intent(in) :: k
+			CALL READ_FILE(k)
+        	CALL READ_DENSITY(k)
+        	CALL READ_U_VELOCITY(k)
+        	CALL READ_V_VELOCITY(k)
+        	CALL READ_W_VELOCITY(k)
+        	CALL READ_PRESSURE(k)
+        	CALL READ_ENERGY(k)
+    	    CALL READ_VORTICITY(k)
+	        CALL READ_LIGHTHILL_SOURCE(k)
+			CALL READ_INFLOW_FREUND(k)
+	
+		END SUBROUTINE READFILE
+	
+	
+		SUBROUTINE READMEAN(k)
+			integer, intent(in) :: k
+			CALL READ_MEAN_DENSITY(k)
+      		CALL READ_MEAN_U_VELOCITY(k)
+        	CALL READ_MEAN_V_VELOCITY(k)
+        	CALL READ_MEAN_W_VELOCITY(k)
+        	CALL READ_MEAN_PRESSURE(k)
+        	CALL READ_MEAN_ENERGY(k)
+        	CALL READ_MEAN_LIGHTHILL_SOURCE(k)
+        	CALL READ_MEAN_CROSS_MOMENTUM(k)
+        	CALL READ_MEAN_RHOU_MOMENTUM(k)
+        	CALL READ_MEAN_RHOV_MOMENTUM(k)
+        	CALL READ_MEAN_RHOW_MOMENTUM(k)
+        	CALL READ_MEAN_TURB_KE(k)
+        	CALL READ_TOTAL(k)
+		
+		END SUBROUTINE READMEAN
+		
+		SUBROUTINE WRITEFILE(k)
+			integer, intent(in) :: k
+			CALL WRITE_INFLOW_FREUND(k)
+			CALL WRITE_DENSITY(k)
+			CALL WRITE_U_VELOCITY(k)
+			CALL WRITE_V_VELOCITY(k)
+			CALL WRITE_W_VELOCITY(k)
+			CALL WRITE_PRESSURE(k)
+			CALL WRITE_ENERGY(k)
+			CALL WRITE_VORTICITY(k)
+			CALL WRITE_LIGHTHILL_SOURCE(k)
+
+			CALL WRITE_DENSITY_XY_PROFILE(k)
+			CALL WRITE_U_VELOCITY_XY_PROFILE(k)
+			CALL WRITE_V_VELOCITY_XY_PROFILE(k)
+			CALL WRITE_W_VELOCITY_XY_PROFILE(k)
+			CALL WRITE_PRESSURE_XY_PROFILE(k)
+			CALL WRITE_ENERGY_XY_PROFILE(k)
+			CALL WRITE_LIGHTHILL_SOURCE_XY_PROFILE(k)
+			CALL WRITE_TEMPERATURE_XY_PROFILE(k)
+			CALL WRITE_VORTICITY_XY_PROFILE(k)
+
+			CALL WRITE_DENSITY_XZ_PROFILE(k)
+			CALL WRITE_U_VELOCITY_XZ_PROFILE(k)
+			CALL WRITE_V_VELOCITY_XZ_PROFILE(k)
+			CALL WRITE_W_VELOCITY_XZ_PROFILE(k)
+			CALL WRITE_PRESSURE_XZ_PROFILE(k)
+			CALL WRITE_ENERGY_XZ_PROFILE(k)
+			CALL WRITE_VORTICITY_XZ_PROFILE(k)
+
+			CALL WRITE_DENSITY_YZ_PROFILE(k)
+			CALL WRITE_U_VELOCITY_YZ_PROFILE(k)
+			CALL WRITE_V_VELOCITY_YZ_PROFILE(k)
+			CALL WRITE_W_VELOCITY_YZ_PROFILE(k)
+			CALL WRITE_PRESSURE_YZ_PROFILE(k)
+			CALL WRITE_ENERGY_YZ_PROFILE(k)
+			CALL WRITE_VORTICITY_YZ_PROFILE(k)
+			CALL WRITE_INFLOW_FREUND(k)
+
+			CALL CALCULATE_MONITORPOINTS(mi)
+			CALL CALCULATE_MONITORLINES(mi)
+			CALL CALCULATE_LIGHTHILL(k)
+
+			WRITE(*,*) 'STEP',k,'     COMPLETED...   FILES ARE WRITTEN'
+		
+		END SUBROUTINE WRITEFILE
+		
+		
+		SUBROUTINE WRITEMEAN(k)
+			integer, intent(in) :: k
+			CALL MEAN_VALUE(k)
+			CALL MEAN_SQUARE_VALUE(k)
+			!
+			CALL WRITE_MEAN_DENSITY(k)
+			CALL WRITE_MEAN_U_VELOCITY(k)
+			CALL WRITE_MEAN_V_VELOCITY(k)
+			CALL WRITE_MEAN_W_VELOCITY(k)
+			CALL WRITE_MEAN_PRESSURE(k)
+			CALL WRITE_MEAN_ENERGY(k)
+			CALL WRITE_MEAN_LIGHTHILL_SOURCE(k)
+			CALL WRITE_MEAN_CROSS_MOMENTUM(k)
+			CALL WRITE_MEAN_RHOU_MOMENTUM(k)
+			CALL WRITE_MEAN_RHOV_MOMENTUM(k)
+			CALL WRITE_MEAN_RHOW_MOMENTUM(k)
+			CALL WRITE_MEAN_TURB_KE(k)
+			!
+			CALL READ_TOTAL(k)
+			!
+			CALL WRITE_MONITORPOINTS(k)
+			CALL WRITE_MONITORLINES(k)
+			WRITE(*,*) 'MEAN DATA WRITTEN FOR',k,'SAMPLES'
+		
+		END SUBROUTINE WRITEMEAN
